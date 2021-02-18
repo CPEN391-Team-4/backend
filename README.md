1131d322e18f9188eefc6855c06a239d9de9781a
John Ramsden <john@ubc.ramsdenj.ca>
2021-02-18 11:34:36 -0800

    Add 'AddTrustedUser' protobuf

--- README.md
+++ README.md
@@ -5,7 +5,13 @@ A gRPC based api.
 ## Protobuf Generation
 
 ```shell
-protoc --go_out=pb --go_opt=paths=source_relative \  john@wooly
+export GO111MODULE=on 
+go get google.golang.org/protobuf/cmd/protoc-gen-go \
+         google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+protoc --go_out=pb --go_opt=paths=source_relative \
     --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
     proto/route.proto
 ```
--- src/server.go
+++ src/server.go
@@ -1,18 +1,72 @@
 package main
 
 import (
-	"context"
+	pb "github.com/CPEN391-Team-4/backend/pb/proto"
 	"google.golang.org/grpc"
-	"github.com/golang/protobuf/proto"
-	"github.com/CPEN391-Team-4/backend/pb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"io"
+	"log"
+	"net"
 )
+
+func logError(err error) error {
+	if err != nil {
+		log.Print(err)
+	}
+	return err
+}
 
 type routeServer struct {
 	pb.UnimplementedRouteServer
 }
 
-func (s *routeServer) GetUsers(context.Context, *pb.UsersParams) (*pb.Users, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetUsers not implemented")
+func (rs *routeServer) AddTrustedUser(stream pb.Route_AddTrustedUserServer) error {
+
+	var user *pb.User
+	imageSize := 0
+	for chunkNum := 0;; chunkNum++ {
+		log.Println("waiting to receive more data")
+
+		req, err := stream.Recv()
+		if err == io.EOF {
+			log.Print("no more data")
+			break
+		}
+		if err != nil {
+			return logError(status.Errorf(codes.Unknown, "cannot receive chunk data: %v", err))
+		}
+
+		if chunkNum == 0 {
+			user = req
+			log.Print("received a user", user)
+		}
+
+		chunk := req.GetImage().Image
+		size := len(chunk)
+
+		log.Printf("received a chunk with size: %d", size)
+		log.Print(chunk)
+
+		imageSize += size
+	}
+
+	return nil
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	rs := routeServer{}
+	pb.RegisterRouteServer(grpcServer, &rs)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
 }
