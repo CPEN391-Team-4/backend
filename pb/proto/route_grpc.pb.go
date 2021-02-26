23d3dc8cccc80e9ec34d87c2f44e8afa7a553e64
Brendonchenhao <Suneduxch@gmail.com>
2021-02-25 16:02:03 -0800

    update simple

--- pb/proto/route_grpc.pb.go
+++ pb/proto/route_grpc.pb.go
@@ -18,7 +18,7 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type RouteClient interface {
-	AddTrustedUser(ctx context.Context, in *User, opts ...grpc.CallOption) (*Empty, error)
+	AddTrustedUser(ctx context.Context, opts ...grpc.CallOption) (Route_AddTrustedUserClient, error)
 }
 
 type routeClient struct {
@@ -29,20 +29,45 @@ func NewRouteClient(cc grpc.ClientConnInterface) RouteClient {
 	return &routeClient{cc}
 }
 
-func (c *routeClient) AddTrustedUser(ctx context.Context, in *User, opts ...grpc.CallOption) (*Empty, error) {
-	out := new(Empty)
-	err := c.cc.Invoke(ctx, "/route.Route/AddTrustedUser", in, out, opts...)
+func (c *routeClient) AddTrustedUser(ctx context.Context, opts ...grpc.CallOption) (Route_AddTrustedUserClient, error) {
+	stream, err := c.cc.NewStream(ctx, &Route_ServiceDesc.Streams[0], "/route.Route/AddTrustedUser", opts...)
 	if err != nil {
 		return nil, err
 	}
-	return out, nil
+	x := &routeAddTrustedUserClient{stream}
+	return x, nil
+}
+
+type Route_AddTrustedUserClient interface {
+	Send(*User) error
+	CloseAndRecv() (*Empty, error)
+	grpc.ClientStream
+}
+
+type routeAddTrustedUserClient struct {
+	grpc.ClientStream
+}
+
+func (x *routeAddTrustedUserClient) Send(m *User) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *routeAddTrustedUserClient) CloseAndRecv() (*Empty, error) {
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	m := new(Empty)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
 
 // RouteServer is the server API for Route service.
 // All implementations must embed UnimplementedRouteServer
 // for forward compatibility
 type RouteServer interface {
-	AddTrustedUser(context.Context, *User) (*Empty, error)
+	AddTrustedUser(Route_AddTrustedUserServer) error
 	mustEmbedUnimplementedRouteServer()
 }
 
@@ -50,8 +75,8 @@ type RouteServer interface {
 type UnimplementedRouteServer struct {
 }
 
-func (UnimplementedRouteServer) AddTrustedUser(context.Context, *User) (*Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method AddTrustedUser not implemented")
+func (UnimplementedRouteServer) AddTrustedUser(Route_AddTrustedUserServer) error {
+	return status.Errorf(codes.Unimplemented, "method AddTrustedUser not implemented")
 }
 func (UnimplementedRouteServer) mustEmbedUnimplementedRouteServer() {}
 
@@ -66,22 +91,30 @@ func RegisterRouteServer(s grpc.ServiceRegistrar, srv RouteServer) {
 	s.RegisterService(&Route_ServiceDesc, srv)
 }
 
-func _Route_AddTrustedUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(User)
-	if err := dec(in); err != nil {
+func _Route_AddTrustedUser_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(RouteServer).AddTrustedUser(&routeAddTrustedUserServer{stream})
+}
+
+type Route_AddTrustedUserServer interface {
+	SendAndClose(*Empty) error
+	Recv() (*User, error)
+	grpc.ServerStream
+}
+
+type routeAddTrustedUserServer struct {
+	grpc.ServerStream
+}
+
+func (x *routeAddTrustedUserServer) SendAndClose(m *Empty) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *routeAddTrustedUserServer) Recv() (*User, error) {
+	m := new(User)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
 		return nil, err
 	}
-	if interceptor == nil {
-		return srv.(RouteServer).AddTrustedUser(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/route.Route/AddTrustedUser",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(RouteServer).AddTrustedUser(ctx, req.(*User))
-	}
-	return interceptor(ctx, in, info, handler)
+	return m, nil
 }
 
 // Route_ServiceDesc is the grpc.ServiceDesc for Route service.
@@ -90,12 +123,13 @@ func _Route_AddTrustedUser_Handler(srv interface{}, ctx context.Context, dec fun
 var Route_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "route.Route",
 	HandlerType: (*RouteServer)(nil),
-	Methods: []grpc.MethodDesc{
+	Methods:     []grpc.MethodDesc{},
+	Streams: []grpc.StreamDesc{
 		{
-			MethodName: "AddTrustedUser",
-			Handler:    _Route_AddTrustedUser_Handler,
+			StreamName:    "AddTrustedUser",
+			Handler:       _Route_AddTrustedUser_Handler,
+			ClientStreams: true,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
 	Metadata: "proto/route.proto",
 }
--- src/server.go
+++ src/server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"fmt"
-	pb "github.com/CPEN391-Team-4/backend/pb/proto"
-	"google.golang.org/grpc"
+	"io"
 	"log"
 	"net"
+
+	pb "github.com/CPEN391-Team-4/backend/pb/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func logError(err error) error {
@@ -20,11 +24,55 @@ type routeServer struct {
 	pb.UnimplementedRouteServer
 }
 
-func (rs *routeServer) AddTrustedUser(ctx context.Context, u *pb.User) (*pb.Empty, error) {
+func (rs *routeServer) AddTrustedUser(stream pb.Route_AddTrustedUserServer) error {
+	// Referenced: dev.to/techschoolguru/
+	//             upload-file-in-chunks-with-client-streaming-grpc-golang-4loc
 
-	fmt.Println("Recieved", u)
+	imgBytes := bytes.Buffer{}
+	var user *pb.User
+	imageSize := 0
+	for chunkNum := 0; ; chunkNum++ {
+		log.Println("waiting to receive more data")
 
-	return &pb.Empty{}, nil
+		req, err := stream.Recv()
+		if err == io.EOF {
+			log.Print("no more data")
+			break
+		}
+		if err != nil {
+			return logError(status.Errorf(codes.Unknown, "cannot receive chunk data: %v", err))
+		}
+
+		if chunkNum == 0 {
+			if req == nil {
+				return logError(status.Errorf(codes.Unknown, "User must be set on first request"))
+			}
+			user = req
+			log.Print("received a user", user)
+		}
+
+		photo := req.GetImage()
+		if photo != nil {
+			chunk := photo.GetImage()
+			size := len(chunk)
+
+			log.Printf("received a chunk with size: %d", size)
+			log.Print(chunk)
+
+			_, err = imgBytes.Write(chunk)
+			if err != nil {
+				return logError(status.Errorf(codes.Internal, "cannot write chunk data: %v", err))
+			}
+
+			imageSize += size
+		}
+
+	}
+
+	fmt.Println(imageSize)
+
+	return nil
+
 }
 
 func main() {
