2cf23951c6c2c372763fa7effa51d38c8f7e7ba0
Sam Schweigel <s.schweigel@gmail.com>
2021-04-12 00:32:15 -0700

    Improve notification message

--- src/server/facialrecognition.go
+++ src/server/facialrecognition.go
@@ -206,7 +206,7 @@ func (rs *routeServer) VerifyUserFace(stream pb.Route_VerifyUserFaceServer) erro
 			return logging.LogError(status.Errorf(codes.Internal, "cannot get tokens: %v", err))
 		}
 		for _, t := range tokens {
-			_, err = notification.Send(t, "Detected human motion", fmt.Sprintf("user=%s", resp.User), rs.firebaseKeyfile)
+			_, err = notification.Send(t, "Detected and let in a trusted person.", resp.User, rs.firebaseKeyfile)
 			if err != nil {
 				_ = logging.LogError(status.Errorf(codes.Internal, "cannot send notification: %v", err))
 			}
