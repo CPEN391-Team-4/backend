5c298321048ce37ad3591ff82e1443775bd38aba
John Ramsden <john@ubc.ramsdenj.ca>
2021-04-13 18:34:21 -0700

    Comment door

--- src/server/door.go
+++ src/server/door.go
@@ -7,41 +7,34 @@ import (
 	"log"
 )
 
+// Flags for door state
 type UnlockDoorRequest struct {
 	requested chan bool
 	done      chan bool
 }
 
+// LockDoor Set the lock/unlock door request and respond when complete
 func (rs *routeServer) LockDoor(ctx context.Context, req *pb.LockDoorReq) (*pb.LockResp, error) {
-	log.Printf("Lock request received: %v", req)
 	rs.unlockDoorRequest.requested <- req.Locked
-	log.Printf("Lock request fin: %v", req)
-	log.Printf("LockDoor: len(rs.unlockDoorRequest.requested)=%v", len(rs.unlockDoorRequest.requested))
 	return &pb.LockResp{Success: <-rs.unlockDoorRequest.done}, nil
 }
 
-//keep sending the video_stream_request state to de1
+// RequestToLock Persistent connection to send lock state to client
 func (rs *routeServer) RequestToLock(stream pb.Route_RequestToLockServer) error {
 	for {
-		log.Printf("RequestToLock")
-		log.Printf("ReqToLock: len(rs.unlockDoorRequest.requested)=%v", len(rs.unlockDoorRequest.requested))
-
 		var req bool
 		select {
 		case <-stream.Context().Done():
-			log.Println("done RequestToLock")
 			return nil
 		case req = <-rs.unlockDoorRequest.requested:
 			break
 		}
-		log.Printf("ReqToLock: len(rs.unlockDoorRequest.requested)=%v", len(rs.unlockDoorRequest.requested))
 		log.Printf("req=%v", req)
 		err := stream.Send(&pb.LockReq{Request: req})
 		if err != nil {
 			return err
 		}
 
-		log.Printf("Sent=%v", req)
 		in, err := stream.Recv()
 		if err != nil {
 			return err
