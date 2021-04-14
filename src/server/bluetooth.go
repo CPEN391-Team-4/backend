67d5445636f5cb8ac40941edd2359cf349e65f3a
John Ramsden <john@ubc.ramsdenj.ca>
2021-04-13 19:30:11 -0700

    Comment bluetooth

--- src/server/bluetooth.go
+++ src/server/bluetooth.go
@@ -3,42 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
 
 	pb "github.com/CPEN391-Team-4/backend/pb/proto"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 const BlueToothTable = "bluetooth"
 
-//update the de1 id and username
-func (rs *routeServer) SendDe1ID(ctx context.Context, in *pb.BluetoothInfo) (*pb.Empty, error) {
-	fmt.Println("receive de1 id and username.")
+/********************* DATABASE *********************/
 
-	err := rs.addBlueinfoTODB(in.Username, in.De1ID)
-	if err != nil {
-		fmt.Println(err)
-		return &pb.Empty{}, err
-	}
-
-	return &pb.Empty{}, nil
-}
-
-//get the de1 id and username
-func (rs *routeServer) GetDe1ID(ctx context.Context, in *pb.MainUser) (*pb.BluetoothInfo, error) {
-	fmt.Println("receive request to get de1 id.")
-	id, err := rs.getDe1IDFromDB(in.Username)
-
-	info := &pb.BluetoothInfo{De1ID: id}
-	if err != nil {
-		fmt.Println(err)
-		return &pb.BluetoothInfo{}, err
-	}
-	return info, nil
-}
-
-//add the username and the de1device id into the bluetooth table
+// addBlueinfoTODB Add the username and the de1device id into BlueToothTable
 func (rs *routeServer) addBlueinfoTODB(username string, de1id string) error {
 	sql_q := fmt.Sprintf(
 		"INSERT INTO `%s` (username, de1id ) VALUES ('%s', '%s');",
@@ -51,7 +25,7 @@ func (rs *routeServer) addBlueinfoTODB(username string, de1id string) error {
 	return err
 }
 
-//by giving the username, get the device id of that username.
+// getDe1IDFromDB Get the device id of username.
 func (rs *routeServer) getDe1IDFromDB(username string) (string, error) {
 	sql_q := "SELECT de1id FROM " + BlueToothTable + " where username = " + "'" + username + "'"
 	fmt.Println(sql_q)
@@ -72,3 +46,25 @@ func (rs *routeServer) getDe1IDFromDB(username string) (string, error) {
 
 	return de1id, err
 }
+
+/********************* BLUETOOTH API *********************/
+
+// SendDe1ID Update the de1 id and username
+func (rs *routeServer) SendDe1ID(ctx context.Context, in *pb.BluetoothInfo) (*pb.Empty, error) {
+	err := rs.addBlueinfoTODB(in.Username, in.De1ID)
+	if err != nil {
+		return &pb.Empty{}, err
+	}
+
+	return &pb.Empty{}, nil
+}
+
+// GetDe1ID Get the de1 id and username
+func (rs *routeServer) GetDe1ID(ctx context.Context, in *pb.MainUser) (*pb.BluetoothInfo, error) {
+	id, err := rs.getDe1IDFromDB(in.Username)
+	if err != nil {
+		return &pb.BluetoothInfo{}, err
+	}
+	return &pb.BluetoothInfo{De1ID: id}, nil
+}
+
