e7c353141fabf9f42269c578d8c895b36db951a1
John Ramsden <john@ubc.ramsdenj.ca>
2021-04-13 19:24:59 -0700

    Comment devices

--- src/server/device.go
+++ src/server/device.go
@@ -3,49 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	pb "github.com/CPEN391-Team-4/backend/pb/proto"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// Table for devices
 const DeviceTable = "devices"
 
-//A grpc call to update the device token of the a certain device
-func (rs *routeServer) UpdateDeviceToken(ctx context.Context, device *pb.DeviceVerify) (*pb.Empty, error) {
+/********************* DATABASE *********************/
 
-	var err error
-
-	// check if the device is already existed
-	find, err := rs.CheckDeviceExisted(device.Deviceid)
-	if err != nil {
-		return &pb.Empty{}, err
-	}
-
-	// is already existed, than just ned to update the token, if not just add a new entry into the device table
-	if find {
-		fmt.Println("update old device token")
-		err := rs.UpdateToken(device.Deviceid, device.Token)
-		if err != nil {
-			return &pb.Empty{}, err
-		}
-	} else {
-		fmt.Println("add new device and its token")
-		err := rs.AddTODeviceDB(device.Deviceid, device.Token)
-		if err != nil {
-			return &pb.Empty{}, err
-		}
-
-	}
-	return &pb.Empty{}, err
-}
-
-//.
-//.
-//.
-//.
-//All the functions for the communication between server to database for history record part.
-
-// add new entry to device table
+// AddTODeviceDB Add new entry to device table
 func (rs *routeServer) AddTODeviceDB(deviceID string, token string) error {
 
 	sql_q := fmt.Sprintf(
@@ -59,7 +28,7 @@ func (rs *routeServer) AddTODeviceDB(deviceID string, token string) error {
 	return err
 }
 
-//check if the device id is already existed in the device table
+// CheckDeviceExisted Check if the device id is already existed in the device table
 func (rs *routeServer) CheckDeviceExisted(deviceID string) (bool, error) {
 	find := true
 	sql_q := "SELECT exists( select * from " + DeviceTable + " WHERE deviceid = ?)"
@@ -84,7 +53,7 @@ func (rs *routeServer) CheckDeviceExisted(deviceID string) (bool, error) {
 	return find, err
 }
 
-//update the device token of the a certain device id
+// UpdateToken Update the device token of the a certain device id
 func (rs *routeServer) UpdateToken(deviceID string, token string) error {
 	sql_q := fmt.Sprintf(
 		"UPDATE `%s` SET token = '%s' where deviceid = '%s';",
@@ -93,7 +62,7 @@ func (rs *routeServer) UpdateToken(deviceID string, token string) error {
 	return err
 }
 
-//get all the token string in the device table
+// GetAllTokens Gget all the tokens in the device table
 func (rs *routeServer) GetAllTokens() ([]string, error) {
 	sql_q := "SELECT token FROM " + DeviceTable
 	res, err := rs.conn.Query(sql_q)
@@ -114,4 +83,35 @@ func (rs *routeServer) GetAllTokens() ([]string, error) {
 	}
 
 	return tokens, err
+}
+
+/********************* DEVICE API *********************/
+
+// UpdateDeviceToken update the device token of the a certain device
+func (rs *routeServer) UpdateDeviceToken(ctx context.Context, device *pb.DeviceVerify) (*pb.Empty, error) {
+
+	var err error
+
+	find, err := rs.CheckDeviceExisted(device.Deviceid)
+	if err != nil {
+		return &pb.Empty{}, err
+	}
+
+	if find {
+		// Token already existed, update the token,
+		log.Println("update old device token")
+		err := rs.UpdateToken(device.Deviceid, device.Token)
+		if err != nil {
+			return &pb.Empty{}, err
+		}
+	} else {
+		// Add a new entry into the device table
+		log.Println("add new device and its token")
+		err := rs.AddTODeviceDB(device.Deviceid, device.Token)
+		if err != nil {
+			return &pb.Empty{}, err
+		}
+
+	}
+	return &pb.Empty{}, err
 }
